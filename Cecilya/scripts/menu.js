392620bf4f2ec28285b59c1fa324289657cb8efb
Davi <franciscodavi327@gmail.com>
2025-02-20 14:33:13 -0300

    Faltando responsividade

---
+++ Cecilya/scripts/menu.js
@@ -0,0 +1,11 @@
+function OpenCloseMenu() {
+    menu.classList.toggle('open');
+}
+
+
+const menu = document.querySelector('header nav');
+const btnOpen = document.querySelector('header #btn-menu-open');
+const btnClose = document.querySelector('header nav #btn-menu-close');
+
+btnOpen.onclick = OpenCloseMenu;
+btnClose.onclick = OpenCloseMenu;
